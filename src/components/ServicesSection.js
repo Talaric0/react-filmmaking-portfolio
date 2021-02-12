997d617b1257408edd70cae749faa3bb96cf3f5c
bruno talarico <bruno.talarico@gmail.com>
2021-02-12 16:51:22 +0000

    services styling, add inter font
    Signed-off-by: bruno talarico <bruno.talarico@gmail.com>

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -5,51 +5,85 @@ import diaphragm from '../img/diaphragm.svg'
 import money from '../img/money.svg'
 import teamwork from '../img/teamwork.svg'
 import home2 from '../img/home2.png'
+//Style
+import styled from 'styled-components'
+import {About, Description, Image} from '../styles'
+
 
 export default function ServicesSection() {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>High <span>quality</span> services</h2>
-        <div className="cards">
+        <Cards>
           
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={clock} alt="Clock" srcset=""/>
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={teamwork} alt="teamwork" srcset=""/>
               <h3>Teamwork</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={diaphragm} alt="diaphragm" srcset=""/>
               <h3>Diaphragm</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={money} alt="affordable" srcset=""/>
               <h3>Affordable</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
 
-        </div>
-      </div>
-      <div className="image">
+        </Cards>
+      </Description>
+      <Image>
         <img src={home2} alt="home" srcset=""/>
-      </div>
-    </div>
+      </Image>
+    </Services>
   )
 }
+
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  @media (max-width: 1300px) {
+    justify-content: center;
+  }
+`;
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
