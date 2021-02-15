c79e92ec51941bfc9dc3e09cae5b843789b3f039
bruno talarico <bruno.talarico@gmail.com>
2021-02-15 13:43:51 +0000

    movie details data, each movies pages layout
    Signed-off-by: bruno talarico <bruno.talarico@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React from 'react'
 import {GlobalStyle} from "./components/GlobalStyle";
 //Import components
 import AboutUs from './pages/AboutUs'
+import ContactUs from './pages/ContactUs'
+import OurWork from './pages/OurWork'
 import Nav from './components/Nav'
+import MovieDetail from './pages/MovieDetail'
+//Router
+import {Switch, Route} from 'react-router-dom'
 
 
 function App() {
@@ -11,7 +16,25 @@ function App() {
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
+
     </div>
   );
 }
