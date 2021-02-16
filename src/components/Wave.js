4216e2538e7844d58cd561b989f160fd643c6814
bruno talarico <bruno.talarico@gmail.com>
2021-02-16 13:22:17 +0000

    wave animation, our work animation
    Signed-off-by: bruno talarico <bruno.talarico@gmail.com>

--- src/components/Wave.js
+++ src/components/Wave.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
-const Wave = () => {
+export default function Wave() {
   return (
     <WaveSvg
       viewBox="0 0 1440 363"
@@ -12,10 +12,10 @@ const Wave = () => {
       <motion.path
         initial={{ pathLength: 0, pathOffset: 1 }}
         animate={{ pathLength: 1, pathOffset: 0 }}
-        transition={{ duration: 1 }}
+        transition={{ duration: 1.5 }}
         d="M1440 27.4774C1352.73 19.8184 1122.41 49.0556 899.331 227.276C620.48 450.052 354.282 355.647 170.328 185.318C23.165 49.0556 -4.21721 8.32998 0.487081 5"
         stroke="#D96ED4"
-        strokeOpacity="0.5"
+        strokeOpacity="0.4"
         strokeWidth="10"
       />
     </WaveSvg>
@@ -27,5 +27,3 @@ const WaveSvg = styled.svg`
   left: 0;
   z-index: 1;
 `;
-
-export default Wave;
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -7,7 +7,7 @@ import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
 //animations
 import {motion} from 'framer-motion'
-import {pageAnimation} from '../animation'
+import {sliderContainer, slider, pageAnimation, fade, photoAnim, lineAnim} from '../animation'
 
 export default function OurWork() {
   return (
@@ -17,12 +17,20 @@ export default function OurWork() {
      animate="show"
      exit="exit" 
     >
-
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
+      
       <Movie>
-        <h2>The Athlete</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>The Athlete</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-athlete">
-          <img src={athlete} alt="athlete"/>
+          <Hide>
+            <motion.img variants={photoAnim} src={athlete} alt="athlete"/>
+          </Hide>
         </Link>
       </Movie>
 
@@ -75,4 +83,24 @@ const Movie = styled.div`
 `;
 const Hide = styled.div`
   overflow: hidden;
+`;
+
+//Frame Animation
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
 `;
